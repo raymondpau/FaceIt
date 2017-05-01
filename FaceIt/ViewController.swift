2dd0486d53a4592197c84531b0cb8ff6dd2547b0
Raymond Pau <rpau@yahoo.com>
2017-05-01 12:34:32 +0800

    Lecture 4 demo code.

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Raymond Pau on 1/5/17.
+//  Copyright © 2017 Raymond Pau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+}
+
